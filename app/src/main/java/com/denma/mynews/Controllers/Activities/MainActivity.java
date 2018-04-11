16e04a1d8e573ba1b16d75a7bb0d39d12b7893e7
Denmaaa <anthony.mineo.pro@Gmail.com>
2018-04-11 17:37:59 +0200

    Project creation, binding with travis

---
+++ app/src/main/java/com/denma/mynews/Controllers/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.denma.mynews.Controllers.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.denma.mynews.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
