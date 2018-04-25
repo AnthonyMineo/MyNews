6f861d793dc8dbe9aad54467963caa2723885da9
Denmaaa <anthony.mineo.pro@Gmail.com>
2018-04-25 17:23:42 +0200

    ...

--- app/src/main/java/com/denma/mynews/Controllers/Fragments/SearchFragment.java
+++ app/src/main/java/com/denma/mynews/Controllers/Fragments/SearchFragment.java
@@ -3,9 +3,7 @@ package com.denma.mynews.Controllers.Fragments;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
-import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
